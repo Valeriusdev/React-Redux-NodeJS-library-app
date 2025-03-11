67022fd5c70b7150b7c4658218005071bd1ab690
Valeriusdev <kakao757@gmail.com>
2025-03-10 19:09:30 -0700

    update Booklist component

--- client/src/components/BookList/Booklist.js
+++ client/src/components/BookList/Booklist.js
@@ -1,6 +1,8 @@
+import { useSelector } from "react-redux";
 import React from "react";
 
 const Booklist = () => {
+  const books = useSelector((state) => state.books);
   return <div className="app-block">Book list</div>;
 };
 
