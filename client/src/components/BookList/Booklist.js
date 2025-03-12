af1434e07c7ec59aa6436867216a5faf3f220176
Valeriusdev <kakao757@gmail.com>
2025-03-11 20:48:38 -0700

    update Booklist component

--- client/src/components/BookList/Booklist.js
+++ client/src/components/BookList/Booklist.js
@@ -3,7 +3,24 @@ import React from "react";
 
 const Booklist = () => {
   const books = useSelector((state) => state.books);
-  return <div className="app-block">Book list</div>;
+  return (
+    <div className="app-block">
+      <h2>Book list</h2>
+      {books.lenght === 0 ? (
+        <p>No books available</p>
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={i}>
+              <div>
+                {book.title} by {book.author}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Booklist;
