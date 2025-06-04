5f56ec36ff5d3581ab5c4041b1424fc10b2f8c47
Valeriusdev <kakao757@gmail.com>
2025-06-04 02:03:19 -0700

    update Filter.js

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Filter = () => {
-  return <div className="app-block">Filters</div>;
+  return (
+    <div className="app-block">
+      <input type="text" placeholder="Filter by title..." />
+    </div>
+  );
 };
 
 export default Filter;
